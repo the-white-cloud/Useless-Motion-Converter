4cd979fe6adb5386ec47038d14017dedd69e1503
TheSamB <82512014+the-white-cloud@users.noreply.github.com>
2022-06-18 10:28:09 +0000

    made time in order of size

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function outputChange() {
   
   document.getElementById("num_to_con").value = output;
 }
-////
+/////////////////////////////////////////////////////
 function getValueOfInput() {
   var numberValue = document.getElementById("num_to_con").value;
   return numberValue
@@ -67,6 +67,10 @@ function distanceUnitSwitch(value) {
       break;
     case 'lightyears':
       var convertToMetres = (1/9460730472580760);
+      break;
+    case "lightseconds":
+      // 299,792,458m
+      var convertToMetres = (1/299792458);
       break;
     case 'lutontolondon':
       // 46.73 km
@@ -139,41 +143,80 @@ function timeUnitSwitch(value) {
     // each is the value of one second in its unit 
     // 1000 ms is one sec
     // 1/60 min is one sec
-    case 'millisecond':
-      var convertToSecond = 1000;
-      break;
-    case 'second':
-      var convertToSecond = 1;
-      break;
-    case 'kilosecond':
-      // 1000 secs
-      var convertToSecond = 1/1000;
-      break;
-    case 'megasecond':
-      // 1000000 secs
-      var convertToSecond = 1/1000000;
-      break;
-    case 'gigasecond':
-      // 1000000000 secs
-      var convertToSecond = 1/1000000000;
-      break;
-    case 'terasecond':
-      // 1000000000000 secs
-      var convertToSecond = 1/1000000000000;
-      break;
+    case 'Planck time':
+      var convertToSecond = 1/0.000000000000000000000000000000000000000005390559999;
+      break; 
+    case 'Jiffy':
+      // 3*10^-24 secs
+      // 0.0000000000000000000003
+      var convertToSecond = (1/(3*(10**-24)));
+      break; 
     case 'attosecond':
       // 1/1000000000000000000 secs
       var convertToSecond = 1000000000000000000;
       break;
+    case 'millisecond':
+      var convertToSecond = 1000;
+      break;
+    case 'half-life of oxygen-13':
+      // 8.585 ms
+      var convertToSecond = 116.482236;
+      break; 
+    case 'second':
+      var convertToSecond = 1;
+      break;
+    case 'nanocentury':
+      // 3.155695200 secs
+      // 1 billionth of a century 
+      var convertToSecond = 1/3.155695200;
+      break;
+    case 'actinium-222':
+      // 5 seconds
+      var convertToSecond = 1/5;
+      break;
+    case 'Decasecond':
+      // 10 secs
+      var convertToSecond = 1/10;
+      break; 
     case 'minute':
       var convertToSecond = 1/60;
+      break;
+    case 'Warhol':
+      // 15 mins
+      var convertToSecond = 1/900;
+      break; 
+    case 'kilosecond':
+      // 1000 secs
+      var convertToSecond = 1/1000;
+      break;
+    case 'microcentury':
+      // 52m and 35.6952 secs // 3155.6952 secs
+      // 1 millionth of a century
+      var convertToSecond = (1/3155.6952);
       break;
     case 'hour':
       var convertToSecond = 1/3600;
       break;
+    case 'Cats movie':
+      // 1 hour 50 mins
+      var convertToSecond = 1/6600;
+      break; 
+    case 'caffeine':
+      // 5 hours
+      var convertToSecond = 1/18000;
+      break;
     case 'day':
       var convertToSecond = 1/86400;
       break;
+    case 'megasecond':
+      // 1000000 secs // approx 11 days
+      var convertToSecond = 1/1000000;
+      break;
+    case 'Fortnite':
+      // 14 days
+      // yes we know it is spelled fortnight
+      var convertToSecond = 1/1210000;
+      break;   
     case 'year':
       var convertToSecond = 1/31536000;
       break;
@@ -181,59 +224,22 @@ function timeUnitSwitch(value) {
       // 365.2422 days
       var convertToSecond = 1/31556926.1;
       break;
-    case 'half-life of oxygen-13':
-      // 8.585 ms
-      var convertToSecond = 116.482236;
-      break; 
-    case 'Cats movie':
-      // 1 hour 50 mins
-      var convertToSecond = 1/6600;
-      break;  
-    case 'Stephen Hawking life':
-      // 27824 days
-      var convertToSecond = 1/2403993600; 
-      break;   
-    case 'Planck time':
-      var convertToSecond = 1/0.00000000000000000000000000000000000000000539;
-      break;  
-    case 'Fortnight':
-      // 14 days
-      var convertToSecond = 1/1210000;
-      break;   
-    case 'Decasecond':
-      // 10 secs
-      var convertToSecond = 1/10;
-      break;   
-    case 'Jiffy':
-      // 3*10^-24 secs
-      var convertToSecond = (1/(3*(10**-24)));
-      break; 
-    case 'Warhol':
-      // 15 mins
-      var convertToSecond = 1/900;
-      break; 
-    case 'caffeine':
-      // 5 hours
-      var convertToSecond = 1/18000;
-      break;
-    case 'actinium-222':
-      // 5 seconds
-      var convertToSecond = 1/5;
-      break;
-    case 'nanocentury':
-      // 3.155695200 secs
-      // 1 billionth of a century 
-      var convertToSecond = 1/3.155695200;
-      break;
-    case 'microcentury':
-      // 52m and 35.6952 secs // 3155.6952 secs
-      // 1 millionth of a century
-      var convertToSecond = (1/3155.6952);
-      break;
     case "28/06/2025":
-      // 804,384,000 secs
-      var convertToMetres = 1/804384000;
+      // 804,384,000 secs // 9310 days // approx 25.5 years
+      var convertToSecond = 1/804384000;
       break;
+    case 'gigasecond':
+      // 1000000000 secs
+      var convertToSecond = 1/1000000000;
+      break;
+    case 'Stephen Hawking life':
+      // 27824 days 
+      var convertToSecond = 1/2403993600; 
+      break;
+    case 'terasecond':
+      // 1000000000000 secs
+      var convertToSecond = 1/1000000000000;
+      break; 
     default:
       var convertToSecond = 1;
       console.log(value, "has not be added");
